22f876593468b2ef892b8256987e40a6259aa214
Jason McVetta <jason.mcvetta@gmail.com>
2013-02-09 19:56:02 -0800

    working srom

--- srom.go
+++ srom.go
@@ -4,27 +4,128 @@
 package main
 
 import (
-	"log"
-	"time"
+	"errors"
 	"fmt"
-	// "github.com/jmcvetta/restclient"
+	"log"
+	"net/url"
+	"strconv"
+	"time"
+	// "github.com/darkhelmet/env"
+	"github.com/opesun/goquery"
 	// "github.com/stathat/go"
+	"runtime"
+	"strings"
+)
+
+const (
+	searchBase = "https://encrypted.google.com/search"
 )
 
 func init() {
 	log.SetFlags(log.Ltime | log.Lshortfile)
 }
 
-type Configuration interface {
-	Terms() ([]string, error)
-	Positive() ([]string, error)
-	Negative() ([]string, error)
+// Srom is a Sucks-Rules-O-Meter.
+type Srom struct {
+	Terms    []string
+	Positive []string
+	Negative []string
+	MaxProcs int
 }
 
-type staticConf struct {}
+type job struct {
+	Term      string
+	Timestamp time.Time
+	Positive  int
+	Negative  int
+}
 
-func (sc *staticConf) Terms() ([]string, error) {
-	t := []string{
+func scrape(query string) (hits int, err error) {
+	u, err := url.Parse(searchBase)
+	if err != nil {
+		return -1, err
+	}
+	v := url.Values{}
+	v.Set("q", query)
+	u.RawQuery = v.Encode()
+	nodes, err := goquery.ParseUrl(u.String())
+	if err != nil {
+		return -1, err
+	}
+	resultStats := nodes.Find("#resultStats").Html()
+	s := strings.Split(resultStats, " ")
+	if len(s) != 3 {
+		err = errors.New("Could not parse Google response")
+		return -1, err
+	}
+	countString := s[1]
+	countString = strings.Replace(countString, ",", "", -1)
+	count, err := strconv.Atoi(countString)
+	if err != nil {
+		return -1, err
+	}
+	return count, nil
+}
+
+func (sr *Srom) processQuery(in, out chan *job) {
+	for {
+		j := <-in
+		log.Println(j)
+		j.Timestamp = time.Now()
+		posQuery := buildQuery(j.Term, sr.Positive)
+		negQuery := buildQuery(j.Term, sr.Negative)
+		posCount, err := scrape(posQuery)
+		if err != nil {
+			log.Println("Could not scrape:", err)
+			continue
+		}
+		negCount, err := scrape(negQuery)
+		if err != nil {
+			log.Println("Could not scrape:", err)
+			continue
+		}
+		msg := strings.Repeat("-", 80) + "\n"
+		msg += j.Term + "\n"
+		msg += fmt.Sprintln("\t Positive:", posCount)
+		msg += fmt.Sprintln("\t Negative:", negCount)
+		log.Println(msg)
+		out <- j
+	}
+	log.Println("Bye")
+}
+
+func buildQuery(term string, templates []string) string {
+	q := fmt.Sprintf(templates[0], term)
+	q = fmt.Sprintf("\"%v\"", q)
+	for _, tmpl := range templates[1:] {
+		s := fmt.Sprintf(tmpl, term)
+		s = fmt.Sprintf(" OR \"%v\"", s)
+		q += s
+	}
+	return q
+}
+
+func (sr *Srom) Run() {
+	in := make(chan *job)
+	out := make(chan *job)
+	for i := 0; i < sr.MaxProcs; i++ {
+		log.Println("Starting processQuery()", i)
+		go sr.processQuery(in, out)
+	}
+	for _, t := range sr.Terms {
+		j := new(job)
+		j.Term = t
+		log.Println("Queuing", t)
+		in <- j
+	}
+	for _ = range sr.Terms {
+		<-out
+	}
+}
+
+func main() {
+	sr := Srom{}
+	sr.Terms = []string{
 		"ubuntu",
 		"obama",
 		"linux",
@@ -35,11 +136,7 @@ func (sc *staticConf) Terms() ([]string, error) {
 		"iOS",
 		"ed lee",
 	}
-	return t, nil
-}
-
-func (sc *staticConf) Positive() ([]string, error) {
-	t := []string{
+	sr.Positive = []string{
 		"%v rules",
 		"%v rocks",
 		"%v is awesome",
@@ -47,11 +144,7 @@ func (sc *staticConf) Positive() ([]string, error) {
 		"%v dominates",
 		"love %v",
 	}
-	return t, nil
-}
-
-func (sc *staticConf) Negative() ([]string, error) {
-	t := []string{
+	sr.Negative = []string{
 		"%v sucks",
 		"%v blows",
 		"%v is worthless",
@@ -59,53 +152,6 @@ func (sc *staticConf) Negative() ([]string, error) {
 		"%v doesn't work",
 		"hate %v",
 	}
-	return t, nil
-}
-
-type query struct {
-	Term string
-	PositiveQuery string
-	NegativeQuery string
-	Timestamp time.Time
-	PositiveCount int
-	NegativeCount int
-}
-
-func main() {
-	conf := new(staticConf)
-	terms, err := conf.Terms()
-	if err != nil {
-		log.Panic(err)
-	}
-	positives, err := conf.Positive()
-	if err != nil {
-		log.Panic(err)
-	}
-	negatives, err := conf.Negative()
-	if err != nil {
-		log.Panic(err)
-	}
-	queries := make(chan *query, 4)
-	for _, t := range terms {
-		q := query{
-			Term: t,
-		}
-		q.PositiveQuery = fmt.Sprintf(positives[0], t)
-		q.PositiveQuery = fmt.Sprintf("\"%v\"", q.PositiveQuery)
-		for _, tmpl := range positives[1:] {
-			s := fmt.Sprintf(tmpl, t)
-			s = fmt.Sprintf(" OR \"%v\"", s)
-			q.PositiveQuery += s
-		}
-		q.NegativeQuery = fmt.Sprintf(negatives[0], t)
-		q.NegativeQuery = fmt.Sprintf("\"%v\"", q.NegativeQuery)
-		for _, tmpl := range negatives[1:] {
-			s := fmt.Sprintf(tmpl, t)
-			s = fmt.Sprintf(" OR \"%v\"", s)
-			q.NegativeQuery += s
-		}
-		queries <- &q
-	}
-	log.Println("foobar")
-	log.Println(queries)
+	sr.MaxProcs = runtime.NumCPU() * 4
+	sr.Run()
 }
