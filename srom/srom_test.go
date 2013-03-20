e633fc7c610cfbb052891118e86b6d8896484fbd
Jason McVetta <jason.mcvetta@gmail.com>
2013-03-19 23:04:16 -0700

    Test Query() using dummy search engines

--- srom/srom_test.go
+++ srom/srom_test.go
@@ -4,6 +4,7 @@ package srom
 
 import (
 	"github.com/bmizerany/assert"
+	"github.com/jmcvetta/randutil"
 	"testing"
 )
 
@@ -16,4 +17,52 @@ func TestBuildQuery(t *testing.T) {
 	s := buildQuery(term, templates)
 	expected := "\"foobar rules\" OR \"foobar rocks\""
 	assert.Equal(t, expected, s)
+}
+
+type DummySearchEngine struct {
+	A int // Returned as hits on first call to Query()
+	B int // Returned as hits on second call to Query()
+	s bool
+}
+
+func (d *DummySearchEngine) ServiceName() string {
+	return "Dummy search engine"
+}
+
+func (d *DummySearchEngine) Query(s string) (hits int, err error) {
+	if d.s {
+		d.s = false
+		return d.B, nil
+	}
+	d.s = true
+	return d.A, nil
+}
+
+func TestQuery(t *testing.T) {
+	pos0, _ := randutil.IntRange(0, 999999999)
+	neg0, _ := randutil.IntRange(0, 999999999)
+	pos1, _ := randutil.IntRange(0, 999999999)
+	neg1, _ := randutil.IntRange(0, 999999999)
+	dse0 := DummySearchEngine{
+		A: pos0,
+		B: neg0,
+	}
+	dse1 := DummySearchEngine{
+		A: pos1,
+		B: neg1,
+	}
+	engines := []SearchEngine{
+		&dse0,
+		&dse1,
+	}
+	sr := New(engines, &NilOutput{})
+	sr.Run()
+	ratio, err := sr.Query("foobar")
+	if err != nil {
+		t.Fatal(err)
+	}
+	r0 := float64(neg0) / float64(pos0)
+	r1 := float64(neg1) / float64(pos1)
+	expected := (r0 + r1) / 2.0
+	assert.Equal(t, expected, ratio)
 }
